7d95bfbbf31a11d3b975b46416d346f55ef94a0a
G1ammi <gianmarcocerato@gmail.com>
2020-02-27 11:12:39 +0100

    Corretto DBConferenza

--- DBConferenza/5c_ConventionQL.sql
+++ DBConferenza/5c_ConventionQL.sql
@@ -1,16 +1,16 @@
 
-/*Elenco piani (con relativo numero sale)  usati in una determinata fasciaoraria */
+/*Elenco piani usati in una determinata fasciaoraria */
 
-SELECT Piano.*,Programma.FasciaOraria
+SELECT Piano.NumeroPiano,Programma.FasciaOraria
 FROM Piano,Sala,Programma
 WHERE Programma.fasciaoraria='9:00-10:00'
 AND Piano.NumeroPiano = Sala.NumeroPiano
 AND Sala.NomeSala = Programma.NomeSala
 
 
-/*Elenco Speech con relatore in una determinata Sala */
+/*Elenco Speech (con relatore) nelle varie fasce orarie  in una determinata Sala */
 
-SELECT Sala.NomeSala,Speech.Titolo,Relatore.NomeRelatore
+SELECT Sala.NomeSala,Speech.Titolo,Relatore.NomeRelatore,Programma.FasciaOraria
 FROM Sala,Programma,Speech,Relaziona,Relatore
 WHERE Sala.NomeSala = "azzurro"
 AND Sala.NomeSala = Programma.NomeSala
@@ -19,7 +19,7 @@ AND Programma.IdProgramma = Relaziona.IdProgramma
 AND Relaziona.IdRel = Relatore.IdRel
 
 
-/*Somma partecipanti agli speech in una determinata fascia oraria*/
+/*Somma partecipanti agli speech svolti in una determinata fascia oraria*/
 
 SELECT Speech.Titolo,COUNT(Partecipante.NomePart) AS TotPartecipanti
 FROM Programma,Speech,Partecipante,Partecipa
@@ -28,3 +28,6 @@ AND Speech.IdSpeech = Programma.IdSpeech
 AND Programma.IdProgramma = Partecipa.IdProgramma
 AND Partecipa.IdPartecipante = Partecipante.IdPartecipante
 GROUP BY Speech.Titolo
+
+
+/*Media partecipanti ai vari speech*/
--- DBConferenza/5c_ConventionSQL.sql
+++ DBConferenza/5c_ConventionSQL.sql
@@ -5,7 +5,7 @@ USE 5C_Convention;
 
 CREATE TABLE  Piano  (
    NumeroPiano  int(5) NOT NULL,
-   NumeroSala  int(11) NOT NULL,
+   NumeroSale  int(11) NOT NULL,
    PRIMARY KEY ( NumeroPiano )
 );
 
@@ -14,6 +14,7 @@ CREATE TABLE  Sala  (
    NomeSala  varchar(30) NOT NULL,
    NPostiSala  int(11) NOT NULL,
    NumeroPiano  int(11) NOT NULL,
+   FOREIGN KEY ( NumeroPiano ) REFERENCES  Piano  ( NumeroPiano ),
    PRIMARY KEY ( NomeSala )
 );
 
@@ -30,7 +31,7 @@ CREATE TABLE  Partecipante  (
 
 CREATE TABLE  Speech  (
    IdSpeech  int(10) NOT NULL,
-   Titolo  varchar(30) NOT NULL,
+   Titolo  varchar(50) NOT NULL,
    Argomento  varchar(200) NOT NULL,
    PRIMARY KEY (IdSpeech)
 );
@@ -41,7 +42,9 @@ CREATE TABLE  Programma  (
    FasciaOraria  varchar(30) NOT NULL,
    NomeSala  varchar(30) NOT NULL,
    IdSpeech  int(10) NOT NULL,
-   PRIMARY KEY ( IdProgramma )
+   PRIMARY KEY ( IdProgramma ),
+   FOREIGN KEY ( NomeSala ) REFERENCES  Sala  ( NomeSala ),
+   FOREIGN KEY ( IdSpeech ) REFERENCES  Speech  ( IdSpeech )
 );
 
 CREATE TABLE  Azienda  (
@@ -57,6 +60,7 @@ CREATE TABLE  Relatore  (
    CognomeRel  varchar(30) NOT NULL,
    MailRel  varchar(30) NOT NULL,
    RagioneSocialeAzienda  varchar(30) NOT NULL,
+   FOREIGN KEY ( RagioneSocialeAzienda ) REFERENCES  Azienda  ( RagioneSocialeAzienda ),
    PRIMARY KEY ( IdRel )
 );
 
@@ -64,35 +68,16 @@ CREATE TABLE  Partecipa  (
    IdPartecipante  int(10) NOT NULL,
    IdProgramma  int(10) NOT NULL,
    Presenza  boolean NOT NULL,
+   FOREIGN KEY ( IdPartecipante ) REFERENCES  Partecipante  ( IdPartecipante ),
+   FOREIGN KEY ( IdProgramma ) REFERENCES  Programma  ( IdProgramma ),
    PRIMARY KEY ( IdPartecipante , IdProgramma )
 );
 
 CREATE TABLE  Relaziona  (
    IdRel  int(10)  NOT NULL,
    IdProgramma  int(10)  NOT NULL,
+   FOREIGN KEY ( IdRel ) REFERENCES  Relatore  ( IdRel ),
+   FOREIGN KEY ( IdProgramma ) REFERENCES  Programma  ( IdProgramma ),
    PRIMARY KEY ( IdRel , IdProgramma )
 );
-
-
-ALTER TABLE  Sala 
-  ADD CONSTRAINT FOREIGN KEY ( NumeroPiano ) REFERENCES  Piano  ( NumeroPiano );
-
-
-ALTER TABLE  Programma 
-  ADD CONSTRAINT FOREIGN KEY ( NomeSala ) REFERENCES  Sala  ( NomeSala ),
-  ADD CONSTRAINT FOREIGN KEY ( IdSpeech ) REFERENCES  Speech  ( IdSpeech );
-
-
-ALTER TABLE  Relatore 
-  ADD CONSTRAINT FOREIGN KEY ( RagioneSocialeAzienda ) REFERENCES  Azienda  ( RagioneSocialeAzienda );
-
-
-ALTER TABLE  Partecipa 
-  ADD CONSTRAINT FOREIGN KEY ( IdPartecipante ) REFERENCES  Partecipante  ( IdPartecipante ),
-  ADD CONSTRAINT FOREIGN KEY ( IdProgramma ) REFERENCES  Programma  ( IdProgramma );
-
-ALTER TABLE  Relaziona 
-  ADD CONSTRAINT FOREIGN KEY ( IdRel ) REFERENCES  Relatore  ( IdRel ),
-  ADD CONSTRAINT FOREIGN KEY ( IdProgramma ) REFERENCES  Programma  ( IdProgramma );
-
 
